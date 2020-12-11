e7598d6bc75e1c5d0250ce0099103a7dcda49be6
unknown <astro124578@gmail.com>
2020-12-11 10:44:11 -0700

    Add about, login, signup, and profile route. Move header and footer html to partial ejs files

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ app.get("/", (req, res) => {
   res.render("landing");
 });
 
+// About Route
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// Landing Route
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// Landing Route
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+// Landing Route
+app.get("/users/:id", (req, res) => {
+  res.render("profile");
+});
+
 // // Recipe Show route
 // app.get("/recipes/:id", (req,res) => {
 //     let url = "https://api.spoonacular.com/recipes/" + req.params.id + "/information?apiKey=69eba1cad9c44ee4ac0e44e3ea0a25ef&includeNutrition=false";
