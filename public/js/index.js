727255ad2d1fc5832663e11519ce61baf667c366
unknown <astro124578@gmail.com>
2020-12-10 18:19:05 -0700

    Add api search functionality, display search results in feature section, adjust css to fit random database information

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,83 @@ function toggleNav() {
     mobileNav.classList.add("hidden");
   }
 }
+
+let recipes;
+const searchBtn = document.querySelector(".search__btn");
+
+document.addEventListener("DOMContentLoaded", function () {
+  searchBtn.addEventListener("click", function () {
+    let offset = 0;
+    let cuisineCategory = document.querySelector(".search__category").value;
+    let searchString = document.querySelector(".search__main").value;
+    let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=69eba1cad9c44ee4ac0e44e3ea0a25ef&query=${searchString}&offset=${offset}&number=12&cuisine=${cuisineCategory}`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        recipes = data;
+        populateSearchResults(data);
+      });
+  });
+});
+
+const populateSearchResults = function (searchResults) {
+  let featured = document.querySelector(".featured");
+  featured.innerHTML = "";
+  let h2 = document.createElement("h2");
+  h2.classList.add("featured__heading", "heading-2");
+  h2.textContent = "Results";
+  featured.appendChild(h2);
+  featured.scrollIntoView();
+
+  for (let i = 0; i < searchResults.results.length; i++) {
+    let recipeData;
+
+    fetch(
+      `https://api.spoonacular.com/recipes/${searchResults.results[i].id}/information?apiKey=69eba1cad9c44ee4ac0e44e3ea0a25ef&includeNutrition=false`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        recipeData = data;
+        createRecipeCard();
+      });
+
+    function createRecipeCard() {
+      let divRecipeCard = document.createElement("div");
+      divRecipeCard.classList.add("recipe__card");
+
+      let imgContainer = document.createElement("figure");
+      imgContainer.classList.add("recipe__img-container");
+
+      let img = document.createElement("img");
+      img.src = searchResults.results[i].image;
+      img.alt = `${searchResults.results[i].title} image`;
+      img.classList.add("recipe__img");
+      imgContainer.appendChild(img);
+
+      divRecipeCard.appendChild(imgContainer);
+
+      let recipeDetails = document.createElement("div");
+      recipeDetails.classList.add("recipe__details");
+
+      let recipeName = document.createElement("a");
+      recipeName.href = `recipe/${searchResults.results[i].id}`;
+      recipeName.classList.add("heading-4", "recipe__name");
+      recipeName.textContent = searchResults.results[i].title;
+      recipeDetails.appendChild(recipeName);
+
+      let recipeSource = document.createElement("p");
+      recipeSource.classList.add("recipe__source");
+      recipeSource.textContent = recipeData.sourceName;
+      recipeDetails.appendChild(recipeSource);
+
+      divRecipeCard.appendChild(recipeDetails);
+
+      let featured = document.querySelector(".featured");
+      featured.appendChild(divRecipeCard);
+    }
+  }
+};
